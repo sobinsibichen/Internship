ff7ce8eff7f579f63bf5c352b0765bf8c4e7a2fe
sobinsibichen <124669405+sobinsibichen@users.noreply.github.com>
2023-06-07 06:39:49 -0700

    Add files via upload

---
+++ WinFormsApp1/WinFormsApp1/Form3.Designer.vb
@@ -0,0 +1,319 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        DataGridView1 = New DataGridView()
+        DataGridView2 = New DataGridView()
+        DataGridView3 = New DataGridView()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        Label3 = New Label()
+        Label4 = New Label()
+        TextBox2 = New TextBox()
+        Label5 = New Label()
+        TextBox3 = New TextBox()
+        Label6 = New Label()
+        TextBox4 = New TextBox()
+        Label7 = New Label()
+        TextBox5 = New TextBox()
+        Label8 = New Label()
+        TextBox6 = New TextBox()
+        Button2 = New Button()
+        Button3 = New Button()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(DataGridView2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(DataGridView3, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.ActiveCaption
+        Button1.Font = New Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Button1.Location = New Point(317, 12)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(186, 41)
+        Button1.TabIndex = 0
+        Button1.Text = "Input Output"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = My.Resources.Resources.logo_transpace
+        PictureBox1.Location = New Point(228, 612)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(328, 43)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 1
+        PictureBox1.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Image = My.Resources.Resources.isro_logo
+        PictureBox2.Location = New Point(793, 12)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(161, 141)
+        PictureBox2.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox2.TabIndex = 2
+        PictureBox2.TabStop = False
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(39, 69)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.RowTemplate.Height = 29
+        DataGridView1.Size = New Size(676, 148)
+        DataGridView1.TabIndex = 3
+        ' 
+        ' DataGridView2
+        ' 
+        DataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView2.Location = New Point(39, 255)
+        DataGridView2.Name = "DataGridView2"
+        DataGridView2.RowHeadersWidth = 51
+        DataGridView2.RowTemplate.Height = 29
+        DataGridView2.Size = New Size(676, 170)
+        DataGridView2.TabIndex = 4
+        ' 
+        ' DataGridView3
+        ' 
+        DataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView3.Location = New Point(39, 454)
+        DataGridView3.Name = "DataGridView3"
+        DataGridView3.RowHeadersWidth = 51
+        DataGridView3.RowTemplate.Height = 29
+        DataGridView3.Size = New Size(676, 141)
+        DataGridView3.TabIndex = 5
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(39, 229)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(53, 23)
+        Label1.TabIndex = 6
+        Label1.Text = "Input"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(39, 428)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(68, 23)
+        Label2.TabIndex = 7
+        Label2.Text = "Output"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(749, 200)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(205, 27)
+        TextBox1.TabIndex = 8
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.Location = New Point(721, 174)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(125, 23)
+        Label3.TabIndex = 9
+        Label3.Text = "Manufacturer:"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Label4.Location = New Point(721, 243)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(99, 23)
+        Label4.TabIndex = 10
+        Label4.Text = "Date Code:"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(749, 269)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(205, 27)
+        TextBox2.TabIndex = 11
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Label5.Location = New Point(721, 312)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(61, 23)
+        Label5.TabIndex = 12
+        Label5.Text = "ID No:"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(749, 338)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(205, 27)
+        TextBox3.TabIndex = 13
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Label6.Location = New Point(721, 377)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(97, 23)
+        Label6.TabIndex = 14
+        Label6.Text = "Test Stage:"
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(749, 403)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(205, 27)
+        TextBox4.TabIndex = 15
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Label7.Location = New Point(721, 443)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(63, 23)
+        Label7.TabIndex = 16
+        Label7.Text = "Tester:"
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(749, 469)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(205, 27)
+        TextBox5.TabIndex = 17
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Label8.Location = New Point(721, 508)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(88, 23)
+        Label8.TabIndex = 18
+        Label8.Text = "Serial No:"
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(749, 534)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(205, 27)
+        TextBox6.TabIndex = 19
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = SystemColors.ActiveCaption
+        Button2.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Button2.Location = New Point(642, 612)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(142, 57)
+        Button2.TabIndex = 20
+        Button2.Text = "Export "
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = SystemColors.ActiveCaption
+        Button3.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Button3.Location = New Point(812, 612)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(142, 57)
+        Button3.TabIndex = 21
+        Button3.Text = "Cancel"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Form3
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(988, 711)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(TextBox6)
+        Controls.Add(Label8)
+        Controls.Add(TextBox5)
+        Controls.Add(Label7)
+        Controls.Add(TextBox4)
+        Controls.Add(Label6)
+        Controls.Add(TextBox3)
+        Controls.Add(Label5)
+        Controls.Add(TextBox2)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(DataGridView3)
+        Controls.Add(DataGridView2)
+        Controls.Add(DataGridView1)
+        Controls.Add(PictureBox2)
+        Controls.Add(PictureBox1)
+        Controls.Add(Button1)
+        Name = "Form3"
+        Text = "Input Output"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        CType(DataGridView2, ComponentModel.ISupportInitialize).EndInit()
+        CType(DataGridView3, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents DataGridView3 As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ WinFormsApp1/WinFormsApp1/Form3.vb
@@ -0,0 +1,79 @@
+﻿
+Public Class Form3
+
+    Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = Form2.ComboBox1.Text
+        TextBox2.Text = Form2.TextBox1.Text
+        TextBox3.Text = Form2.TextBox3.Text
+        TextBox4.Text = Form2.ComboBox2.Text
+        TextBox5.Text = Form2.TextBox2.Text
+        TextBox6.Text = Form2.TextBox4.Text
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+
+            Dim MyConnection As System.Data.OleDb.OleDbConnection
+            Dim dataSet As System.Data.DataSet
+            Dim MyCommand As System.Data.OleDb.OleDbDataAdapter
+            Dim path As String = "D:\transpace\Info on i-o.xlsx"
+
+            MyConnection = New System.Data.OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended Properties=Excel 12.0;")
+            MyCommand = New System.Data.OleDb.OleDbDataAdapter("select * from [Sheet1$]", MyConnection)
+
+            dataSet = New System.Data.DataSet
+            MyCommand.Fill(dataSet)
+            DataGridView1.DataSource = dataSet.Tables(0)
+
+            MyConnection.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message.ToString)
+        End Try
+
+
+
+        Try
+
+            Dim MyConnection As System.Data.OleDb.OleDbConnection
+            Dim dataSet As System.Data.DataSet
+            Dim MyCommand As System.Data.OleDb.OleDbDataAdapter
+            Dim path As String = "D:\transpace\INPUT.xlsx"
+
+            MyConnection = New System.Data.OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended Properties=Excel 12.0;")
+            MyCommand = New System.Data.OleDb.OleDbDataAdapter("select * from [Sheet1$]", MyConnection)
+
+            dataSet = New System.Data.DataSet
+            MyCommand.Fill(dataSet)
+            DataGridView2.DataSource = dataSet.Tables(0)
+
+            MyConnection.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message.ToString)
+        End Try
+
+        Try
+
+            Dim MyConnection As System.Data.OleDb.OleDbConnection
+            Dim dataSet As System.Data.DataSet
+            Dim MyCommand As System.Data.OleDb.OleDbDataAdapter
+            Dim path As String = "D:\transpace\output table.xlsx"
+
+            MyConnection = New System.Data.OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended Properties=Excel 12.0;")
+            MyCommand = New System.Data.OleDb.OleDbDataAdapter("select * from [Sheet1$]", MyConnection)
+
+            dataSet = New System.Data.DataSet
+            MyCommand.Fill(dataSet)
+            DataGridView3.DataSource = dataSet.Tables(0)
+
+            MyConnection.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message.ToString)
+        End Try
+
+
+    End Sub
+End Class
